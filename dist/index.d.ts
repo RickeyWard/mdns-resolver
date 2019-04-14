2a240e2b0dca6bd593d24e0def1d1a09bb4b4225
DiamondDrake <RickeyWard@users.noreply.github.com>
2019-04-13 21:55:24 -0400

    Create index.d.ts

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+export declare const resolve: (arg1: string, arg2: "A" | "AAAA" | "PTR" | "TXT" | "SRV" | "HINFO") => Promise<{}>;
+export declare const resolve4: (hostname: string) => Promise<{}>;
+export declare const resolve6: (hostname: string) => Promise<{}>;
+export declare const resolvePtr: (hostname: string) => Promise<{}>;
+export declare const resolveTxt: (hostname: string) => Promise<{}>;
+export declare const resolveSrv: (hostname: string) => Promise<{}>;
+export declare const resolveHinfo: (hostname: string) => Promise<{}>;
